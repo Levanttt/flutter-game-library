7a1a1b673e33882dcc153bc9e35b65167b64afd3
Levanttt <luthfiardi66@gmail.com>
2026-06-10 19:57:40 +0700

    Delete Unused import
    Hapus unused import karna udah dipindahin ui utama nya pada main_page.dart

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'detail_page.dart';
 
 class Game {
   final String name;
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'main_page.dart';
-import 'list_page.dart';
 import 'detail_page.dart';
 import 'avatar_page.dart';
 
